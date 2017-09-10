55c13b10bad7b4840a8280856a6f78ed7da19fb7
Colin McKean <ezikill@hotmail.com>
2017-09-10 17:02:01 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
   def self.find_by_name(song_name)
     self.all.find{|song| song.name == song_name}
   end
-  def find_or_create_by_name(song_name)
+  def self.find_or_create_by_name(song_name)
     create_by_name(song_name) unless find_by_name != nil
   end
 end
